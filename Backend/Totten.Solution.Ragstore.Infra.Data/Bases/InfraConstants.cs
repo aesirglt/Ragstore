b9cb53f0829d9144f04332bb3a224635030378e6
Aleff Moura <aleff_moura@hotmail.com>
2025-01-23 23:51:48 +0000

    change ip and envs

--- Backend/Totten.Solution.Ragstore.Infra.Data/Bases/InfraConstants.cs
+++ Backend/Totten.Solution.Ragstore.Infra.Data/Bases/InfraConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace Totten.Solution.Ragstore.Infra.Data.Bases;
 public class InfraConstants
 {
-    public const string MAIN_IP = "192.168.4.109";
-    public const string STORE_DB_NAME = "RagnaStore";
+    public readonly string MAIN_IP = Environment.GetEnvironmentVariable("MAIN_IP") ?? "192.168.4.109";
+    public readonly string STORE_DB_NAME = Environment.GetEnvironmentVariable("STORE_DB_NAME") ?? "RagnaStore";
 }
