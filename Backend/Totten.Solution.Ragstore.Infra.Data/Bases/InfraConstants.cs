36bbf6059980c8d359ea4914c8656058a87032ec
Aleff Moura <aleff_moura@hotmail.com>
2025-01-24 10:41:59 +0000

    dbname

--- Backend/Totten.Solution.Ragstore.Infra.Data/Bases/InfraConstants.cs
+++ Backend/Totten.Solution.Ragstore.Infra.Data/Bases/InfraConstants.cs
@@ -2,5 +2,5 @@
 public class InfraConstants
 {
     public static readonly string MAIN_IP = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "postgres";
-    public static readonly string STORE_DB_NAME = Environment.GetEnvironmentVariable("POSTGRES_DB") ?? "ragnastore";
+    public static readonly string STORE_DB_NAME = Environment.GetEnvironmentVariable("POSTGRES_DB") ?? "ragstore";
 }
