d33ef2ca68935528b68883b22b627a606e0d34dc
Aleff Moura <aleff_moura@hotmail.com>
2025-01-24 13:52:37 +0000

    endpoint for migrate

--- Backend/Totten.Solution.Ragstore.WebApi/Controllers/UsersController.cs
+++ Backend/Totten.Solution.Ragstore.WebApi/Controllers/UsersController.cs
@@ -1,8 +1,12 @@
 ﻿namespace Totten.Solution.Ragstore.WebApi.Controllers;
 
 using Autofac;
+using FunctionalConcepts;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Totten.Solution.Ragstore.Infra.Data.Contexts.StoreServerContext;
 using Totten.Solution.Ragstore.WebApi.Bases;
+using Totten.Solution.Ragstore.WebApi.SystemConstants;
 
 /// <summary>
 /// Endpoint responsavel por usuários
@@ -17,5 +21,20 @@ public class UsersController : BaseApiController
     /// <param name="lifetimeScope"></param>
     public UsersController(ILifetimeScope lifetimeScope) : base(lifetimeScope)
     {
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="server"></param>
+    /// <returns></returns>
+    [HttpPost("{server}/migrate")]
+    [ProducesResponseType<Success>(statusCode: 201)]
+    public async Task<IActionResult> Create([FromRoute]string server)
+    {
+        var opt = new DbContextOptionsBuilder<ServerStoreContext>().UseNpgsql(SysConstantDBConfig.DEFAULT_CONNECTION_STRING.Replace("{dbName}", server));
+        var ctx = new ServerStoreContext(opt.Options);
+        await ctx.Database.MigrateAsync();
+        return Ok();
     }
 }
